fe8dda2f2abf945bcf66cd7d698f81223c2f5c5d
smartinkcherian <smartincherian@7thpillar.com>
2025-05-20 12:57:18 +0530

    renderToPipeableStream

--- src/server/renderComponent.js
+++ src/server/renderComponent.js
@@ -4,6 +4,7 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const fs = require('fs');
 const path = require('path');
+const { Writable } = require('stream');
 
 // Import all shared components
 const componentsPath = path.resolve(__dirname, '../../shared-components');
@@ -31,8 +32,36 @@ componentFolders.forEach((folder) => {
   }
 });
 
-module.exports = async function renderComponent(name) {
+module.exports = async function renderComponent(name, props = {}) {
   const Component = components[name];
   if (!Component) throw new Error(`Component ${name} not found`);
-  return ReactDOMServer.renderToString(<Component />);
+
+  return new Promise((resolve, reject) => {
+    let html = '';
+
+    const { pipe } = ReactDOMServer.renderToPipeableStream(
+      <Component {...props} />
+    );
+
+    // Create a real Writable stream that collects the HTML
+    const writable = new Writable({
+      write(chunk, encoding, callback) {
+        html += chunk.toString();
+        callback();
+      },
+    });
+
+    writable.on('finish', () => {
+      resolve(html);
+    });
+
+    // Pipe rendered output into our stream
+    pipe(writable);
+
+    // Optional: handle errors
+    writable.on('error', (err) => {
+      console.error('Writable stream error:', err);
+      reject(err);
+    });
+  });
 };
